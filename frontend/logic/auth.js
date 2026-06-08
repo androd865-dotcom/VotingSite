b51492a5f97b88b24cf77d3be9b3b0a717edcc54
Nikosweet <kupchikov1975@mail.ru>
2026-06-08 22:47:37 +0700

    feat(auth): написать скелет авторизации

--- frontend/logic/auth.js
+++ frontend/logic/auth.js
@@ -1,19 +1,38 @@
-async function isAuthorized() {
-
-}
 
 async function login(name, password) {
-    const json = await fetch('http://localhost:3000/api/login', {
+    const response = await fetch('http://localhost:3000/api/login', {
         method: 'POST',
         body: JSON.stringify({name, password})
     })
-    console.log(json)
+    if (response.ok) {
+        if (name === 'admin123') document.querySelector('.header_auth').innerHTML = `<button class="header_auth__button">Создать голосование</button>`
+        else document.querySelector('.header_auth').innerHTML = name;
+    } else console.log('Неверный логин или пароль!')
 }
 
 async function register(name, password) {
-    const json = await fetch('http://localhost:3000/api/register', {
+    if (password.length < 5) {console.log("Этот пароль слишком короткий!"); return;}
+    const response = await fetch('http://localhost:3000/api/register', {
         method: 'POST',
         body: JSON.stringify({name, password})
     })
-    console.log(json)
+    if (response.ok) {
+        document.querySelector('.header_auth').innerHTML = name;
+    } else console.log('Пользователь с таким именем уже существует!');
+
+}
+
+function logout() {
+    document.querySelector('.header_auth').innerHTML = `
+        <li>
+            <button class="header_auth__button">Войти</button>
+        </li>
+        <li>
+            <button class="header_auth__button">Зарегистрироваться</button>
+        </li>
+    `;
+}
+
+function drawAuthorized() {
+
 }
